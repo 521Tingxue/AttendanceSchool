be945127bbe566b7ea0ff8b2afaee704313aa7f2
521Tingxue <1300304687@qq.com>
2016-04-07 14:29:16 +0800

    添加项目

---
+++ AttendanceSchool/AttendanceSchool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AttendanceSchool
+//
+//  Created by 揽梦科技 on 16/4/7.
+//  Copyright © 2016年 揽梦科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AttendanceSchool/AttendanceSchool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AttendanceSchool
+//
+//  Created by 揽梦科技 on 16/4/7.
+//  Copyright © 2016年 揽梦科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
